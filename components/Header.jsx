70734f67aee766937a8f3d5c556baba75607ee51
unknown <brightdaniel5050@gmail.com>
2022-03-14 21:27:27 +0100

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+
+import Link from 'next/link';
+import { getCategories } from '../services';
+
+const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="border-b w-full inline-block border-blue-400 py-8">
+        <div className="md:float-left block">
+          <Link href="/">
+            <span className="cursor-pointer absolute w-auto h-auto top-0 px-0 mx-0  block w-9/12 h-full">
+            <Image
+            src="/../public/home.png"
+            alt="Quest Logo"
+            width="300px"
+            height="150px"
+            />
+            </span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
