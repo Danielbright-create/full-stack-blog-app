261d63b0a81c8c9c0e9846e7f0a3ff89290bd4f8
unknown <brightdaniel5050@gmail.com>
2022-03-15 16:01:53 +0100

    your message

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,8 @@ export default function Home( { posts }) {
     <div className="container mx-auto px-10 mb-8 ">
       <Head>
         <title>Iv Quest </title>
-        <Link rel="icon" type="image/png" href="favicon.png" />
+        <Link rel="icon" type="image/png" href="favicon.png" >
+          </Link>
       </Head>
       < FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12"> 
