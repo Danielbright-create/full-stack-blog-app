97ec0e70e4f582c3454c79751c288257ffa9740f
unknown <brightdaniel5050@gmail.com>
2022-03-19 13:22:28 +0100

    your message

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -15,6 +15,10 @@ const PostDetail = ({ post }) => {
         modifiedText = (<em key={index}>{text}</em>);
       }
 
+      if (obj.link) {
+        modifiedText = (<em key={index}>{text}</em>);
+      }
+
       if (obj.underline) {
         modifiedText = (<u key={index}>{text}</u>);
       }
@@ -23,10 +27,13 @@ const PostDetail = ({ post }) => {
     switch (type) {
       case 'heading-three':
         return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
+
       case 'paragraph':
         return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
+
       case 'heading-four':
         return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
+        
       case 'image':
         return (
           <img
